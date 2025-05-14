f192eaa1e6b31d61afcfe1b6933b4cf79dce7d14
faruqiakbr <faruqi.akbar02@gmail.com>
2025-05-14 20:32:21 +0700

    first

---
+++ src/main/java/com/mycompany/pojekakhirbismillah/PojekAkhirBISMILLAH.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pojekakhirbismillah;
+
+/**
+ *
+ * @author faruq
+ */
+public class PojekAkhirBISMILLAH {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
